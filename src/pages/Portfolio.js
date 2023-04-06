bae1092f2d5ada1741af5803b801bd21b9e486c5
saM041988 <sambitmohanty041988@gmail.com>
2023-04-06 07:43:25 +0530

    first commit

---
+++ src/pages/Portfolio.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Portfolio = () => {
+  return (
+    <>
+        <div className='container'>
+            <h3 className='text-center my-5'>Project Made by me</h3>
+        </div>
+        
+    </>
+  )
+}
+
+export default Portfolio
